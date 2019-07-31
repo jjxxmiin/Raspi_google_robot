64811468946cace866d7c33c9f360ae8e580e37e
jjeamin <woalsdl500@naver.com>
2019-07-31 13:31:57 +0900

    Update shell script

--- stt/google.sh
+++ stt/google.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-~/env/bin/python3 ~/workspace/Raspi_google_robot/stt/pushtotalk.py --mode story > /home/pi/workspace/Raspi_google_robot/stt/t.log 2>&1
+~/env/bin/python3 ~/workspace/Raspi_google_robot/stt/backup.py 
+#> ~/workspace/Raspi_google_robot/stt/t.log 2>&1
